f07fde4ad6e810270c4c2a95b90f63964c382e13
yuliusjati <yulius_jati_28rpl@student.smktelkom-mlg.sch.id>
2021-06-07 08:51:00 -0700

    tugas 6

---
+++ app/Models/Buku.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    protected $table = 'buku';
+    protected $fillable = ['judul', 'penulis', 'penerbit', 'tahun'];
+}
